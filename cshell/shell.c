da657870e9c28cdbd869b877b66633cc22063f45
Saakshaat <saakshaat2001@gmail.com>
2020-12-10 01:12:38 -0800

    Add TODOs for cshell lifecycle

--- cshell/shell.c
+++ cshell/shell.c
@@ -1,6 +1,22 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include "interpret.h"
 
 int main() {
-	return interpretExpression("ls", 3);
+	char* buffer;
+	size_t bufsize = 32;
+	size_t characters;
+
+	buffer = (char*)malloc(bufsize * sizeof(char));
+
+	if(buffer == NULL) {
+		perror("Unable to allocate buffer.");
+		exit(1);
+	}
+
+	printf("Type something:\n");
+	characters = getline(&buffer, &bufsize, stdin);
+
+	interpretExpression(buffer, 3);
+	return 0;
 }
