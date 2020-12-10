20c67992b2809cc8acfcacbd3b70fca28640bfb2
Saakshaat <saakshaat2001@gmail.com>
2020-12-10 02:10:58 -0800

    Update main with non-functional shell loop

--- cshell/main.c
+++ cshell/main.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+
 #include "interpret.h"
 
 int main() {
@@ -13,7 +15,20 @@ int main() {
 	 * 6. Repeat
 	 * 
 	 */
+	printf("Welcome to cshell!\n"); // a function writing welcome to the console will replace this
 
-	interpretExpression("line", 3);
+	// user input - this variable will be removed from the driver
+	char * input;
+
+	do {
+		if(input[0] != '\0') { // if the input isn't empty, evaluate input
+			interpretExpression(input, 3); // parsing, evaluation and execution will occure elsewhere
+		}
+
+		printf("cshell>"); // a nicer prompt should replace this since `printf()` takes up memory itself
+		scanf("%s", input); // a helper in another file will replace this and take care of parsing the entire line
+
+	} while(input[0] != '\0' || input[0] != '\n' || strcmp(input,"^C") != 0 || strcmp(input,"^D") != 0); // FIXME: change to valid checks
+	
 	return 0;
 }
