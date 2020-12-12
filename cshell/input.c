119f21e3b3d71eb36ec798b98fc70db7444c2b0e
Saakshaat <saakshaat2001@gmail.com>
2020-12-12 00:08:01 -0800

    Remove newline character from getUserInput function

--- cshell/input.c
+++ cshell/input.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "input.h"
 
-char* getUserInput()
+char *getUserInput()
 {
     size_t bufsize = 1024;
-    char* buffer = malloc(bufsize);
+    char *buffer = malloc(bufsize);
     getline(&buffer, &bufsize, stdin);
+
+    int length = strlen(buffer);
+
+    if (buffer[length - 1] == '\n')
+    { // delete new-line character
+        buffer[length - 1] = '\0';
+    }
 
     return buffer;
 }
