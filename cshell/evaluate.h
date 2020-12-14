f79fe34ab50b80c427ecf97d6bab536c060f828b
Saakshaat <saakshaat2001@gmail.com>
2020-12-13 20:36:43 -0800

    Delete previous C files

--- cshell/evaluate.h
+++
@@ -1,6 +0,0 @@
-#ifndef EVALUATE_H
-#define EVALUATE_H
-
-int evalDriver(char *inputLine);
-
-#endif
--- cshell/input.h
+++
@@ -1,8 +0,0 @@
-#ifndef INPUT_H
-#define INPUT_H
-
-char* getCurrentDirectory();
-char* getConsolePrompt();
-char* getUserInput();
-
-#endif
